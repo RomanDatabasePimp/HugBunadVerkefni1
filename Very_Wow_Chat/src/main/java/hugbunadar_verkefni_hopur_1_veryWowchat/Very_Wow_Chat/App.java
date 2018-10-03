406f5cf8e700d118ca54626c613b2e37ee7fa707
Roman <roman.rumba@hotmail.com>
2018-10-03 15:13:52 +0000

    test

---
+++ Very_Wow_Chat/src/main/java/hugbunadar_verkefni_hopur_1_veryWowchat/Very_Wow_Chat/App.java
@@ -0,0 +1,13 @@
+package hugbunadar_verkefni_hopur_1_veryWowchat.Very_Wow_Chat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
