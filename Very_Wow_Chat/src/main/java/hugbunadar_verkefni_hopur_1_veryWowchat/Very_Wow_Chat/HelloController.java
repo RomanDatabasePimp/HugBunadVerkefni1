cac39caf5d32c1d89afc2c065f93865e643d2f61
Roman <roman.rumba@hotmail.com>
2018-10-04 08:45:41 +0000

    bætti við alvöru json response dæmi

--- Very_Wow_Chat/src/main/java/hugbunadar_verkefni_hopur_1_veryWowchat/Very_Wow_Chat/HelloController.java
+++ Very_Wow_Chat/src/main/java/hugbunadar_verkefni_hopur_1_veryWowchat/Very_Wow_Chat/HelloController.java
@@ -1,13 +1,21 @@
 package hugbunadar_verkefni_hopur_1_veryWowchat.Very_Wow_Chat;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    
 	@RequestMapping("/hello")
-	public String hello() {
-		return "{\"content\":\"Hello nub\"}";
-	}
+    @ResponseBody
+    public HelloReturner sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new HelloReturner(counter.incrementAndGet(), String.format(template, name));
+    }
 }
---
+++ Very_Wow_Chat/src/main/java/hugbunadar_verkefni_hopur_1_veryWowchat/Very_Wow_Chat/HelloReturner.java
@@ -0,0 +1,21 @@
+package hugbunadar_verkefni_hopur_1_veryWowchat.Very_Wow_Chat;
+
+public class HelloReturner {
+	
+	    private final long id;
+	    private final String content;
+
+	    public HelloReturner(long id, String content) {
+	        this.id = id;
+	        this.content = content;
+	    }
+
+	    public long getId() {
+	        return id;
+	    }
+
+	    public String getContent() {
+	        return content;
+	    }
+
+}
