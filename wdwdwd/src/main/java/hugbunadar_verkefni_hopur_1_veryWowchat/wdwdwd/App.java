3298d7a7c4225f373daac16f822b4898cbf9b491
Roman <roman.rumba@hotmail.com>
2018-10-17 13:09:37 +0000

    dæmi með websocets

---
+++ wdwdwd/src/main/java/hugbunadar_verkefni_hopur_1_veryWowchat/wdwdwd/App.java
@@ -0,0 +1,13 @@
+package hugbunadar_verkefni_hopur_1_veryWowchat.wdwdwd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
