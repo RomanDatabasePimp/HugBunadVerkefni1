9fc043361b6ad014b7618fdb8d0f83f7cd265397
Roman <roman.rumba@hotmail.com>
2018-10-29 20:59:04 +0000

    register controller er 95% tilbuin það er bara smá neo4j ver eftir

--- Very_Wow_Chat/src/main/java/project/controller/RegisterController.java
+++ Very_Wow_Chat/src/main/java/project/controller/RegisterController.java
@@ -4,28 +4,32 @@ import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import project.persistance.entities.HttpResponseBody;
+import project.persistance.entities.User;
 import project.persistance.entities.UserRegistrationFormReceiver;
 import project.services.AuthenticationService;
 import project.services.RedisService;
+import project.services.UserService;
 
-/* This class handles http POST request on url/register with a sent json obj  */
-/* https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html */
+/* This class handles http POST, PUT for registration  of a new user  */
 @RestController
 public class RegisterController {
-  // this contains the clientResponse
-  private HttpResponseBody clientResponse;
-  // get our authenticator to authenticate the data 
-  private AuthenticationService authenticator = new AuthenticationService();
-  // our Bcryptor for the sensitive data
-  private BCryptPasswordEncoder privateInfoEncoder = new BCryptPasswordEncoder();
-  private MailController mailMan;
-  private RedisService redisService = new RedisService();
+
+  private final RedisService redisService; // redis services to insert the user into temp storage
+  private final UserService userService;
+	
+  public RegisterController (UserService userService) {
+	this.redisService = new RedisService();
+	this.userService = userService;
+  }
+  
+  /*--------------------------------------------CONTROLLERS START---------------------------------------------------------*/
   
   /* Usage : url/register
    *  For  : POST request
@@ -37,56 +41,92 @@ public class RegisterController {
    * After : Validates the Client POST request and responds with an appropriate status code along with data */
   // value is endpoint , mothen is to what method this response to , headders denotes what are we returning
   @RequestMapping(value="/register", method = RequestMethod.POST, headers = "Accept=application/json")
-  public ResponseEntity<String> register(@RequestBody UserRegistrationFormReceiver payload) throws Exception {
-	this.clientResponse = new HttpResponseBody(); // create a new instance of the response body
+  public ResponseEntity<String> register(@RequestBody UserRegistrationFormReceiver payload) throws Exception {  
+	HttpResponseBody clientResponse = new HttpResponseBody(); // create a instance of the response body
+	AuthenticationService authenticator = new AuthenticationService();// authenticator to authenticate the received data
 	
-	// check if the payload contains everything we need to proccess this request if it dosent we respond accordingly
+	// this is not for the api more of for debuggin.
     if(!payload.allInfoExists()) {
-      // reason why this is a single Error not a list of errors is cuz this should never happen heroku should catch
-      // this and stop it from sending an empty resource this is more for catching with testing porpuses like postman and stuff
-      this.clientResponse.addSingleError("error","All information must be filled");
-      return new ResponseEntity<>(this.clientResponse.getErrorResponse(), HttpStatus.BAD_REQUEST);
+      /* reason why this is a single Error not a list of errors is cuz this should never happen heroku should catch
+       this and stop it from sending an empty resource this is more for catching with testing porpuses like postman and stuff */
+      clientResponse.addSingleError("error","All information must be filled");
+      return new ResponseEntity<>(clientResponse.getErrorResponse(), HttpStatus.BAD_REQUEST);
     }
     
     /* before starting authenticating we check if the client can have this username
-       i decided if the username is taken there is no reason to validate the other sets of given data */
-    if(this.authenticator.userNameExists(payload.getUserName())) {
-       this.clientResponse.addToErrorArray("errors", "username", "Username already exists");
-       return new ResponseEntity<>(this.clientResponse.getErrorResponse(), HttpStatus.BAD_REQUEST);
+       i decided if the username is taken there is no reason to validate rest of the data */
+    if(authenticator.userNameExists(payload.getUserName())) {
+      clientResponse.addToErrorArray("errors", "username", "Username already exists");
+      return new ResponseEntity<>(clientResponse.getErrorResponse(), HttpStatus.BAD_REQUEST);
     }
     
     // now that we know we have all the data we need and we know the username is legit we can start validating the data
-    if(!this.authenticator.passwordsMach(payload.getPassword(), payload.getPasswordReap())) {
-      this.clientResponse.addToErrorArray("errors", "password", "Both passwords must match");
+    //We start calling out validator to validate the information and if there is a problem we take note of it
+    if(!authenticator.passwordsMach(payload.getPassword(), payload.getPasswordReap())) {
+      clientResponse.addToErrorArray("errors", "password", "Both passwords must match");
     }
-    if(!this.authenticator.validatePass(payload.getPassword())) {
-      this.clientResponse.addToErrorArray("errors", "password", "Password must be  atleast 8 characters long"
+    if(!authenticator.validatePass(payload.getPassword())) {
+      clientResponse.addToErrorArray("errors", "password", "Password must be  atleast 8 characters long"
       		+ "have 1 digit,lower,uppercase,specialcase letter and cannot contain spaces/tabs");
     }
+    
     // if errors exists then we return the errors as the response
-    if(this.clientResponse.errorsExist()) {
-      return new ResponseEntity<>(this.clientResponse.getErrorResponse(), HttpStatus.BAD_REQUEST);
+    if(clientResponse.errorsExist()) {
+      return new ResponseEntity<>(clientResponse.getErrorResponse(), HttpStatus.BAD_REQUEST);
     }
     
-    /* At this point we know that the user data is valid so we proceede creating him in redis and sending him a validation link */
+    /* At this point we know that the user data is valid so we proceede creating him in redis and sending him a validation link 
+     * our Bcryptor for encrypting sensitive data */
+    BCryptPasswordEncoder privateInfoEncoder = new BCryptPasswordEncoder();
+	
     // create a json obj that we can store in redis and add all the information to it along with encrypting the private data
     JSONObject newUser = new JSONObject();
     newUser.put("userName", payload.getUserName());
     newUser.put("displayName", payload.getDisplayName());
-    newUser.put("password", this.privateInfoEncoder.encode(payload.getPassword()));
+    newUser.put("password", privateInfoEncoder.encode(payload.getPassword()));
     // reason why we encode email is cuz of the new privacy policies any data that can lead to the user(as a person) has to be secured
-    newUser.put("email", this.privateInfoEncoder.encode(payload.getEmail()));
+    newUser.put("email", privateInfoEncoder.encode(payload.getEmail()));
     
     // insert the data into Redis for 30 min
     this.redisService.insertUser(payload.getUserName(), newUser);
     
-    // now we can send email to our new user
-    this.mailMan = new MailController(payload.getEmail(),payload.getUserName()); // create the mail package
-    this.mailMan.send(); // send the email to the user 
+    // the mailMan who will call the webServer to send a validation email
+    MailController mailMan = new MailController(payload.getEmail(),payload.getUserName()); // create the mail package
+    mailMan.send(); // send the email to the user 
     
     /* Responde finnally with a happy message that the user needs to validate his account before he can use it*/
-    this.clientResponse.addSingleSucc("success","Registration successful, a  validation link has been send to :"+payload.getEmail());
-    return new ResponseEntity<>(this.clientResponse.getSuccessResponse(), HttpStatus.CREATED);
-	
+    clientResponse.addSingleSucc("success","Registration successful, a  validation link has been send to :"+payload.getEmail());
+    return new ResponseEntity<>(clientResponse.getSuccessResponse(), HttpStatus.CREATED);
   }
+  
+  /* Usage : url/validation/{key}
+   *  For  : PUT request
+   *         key is a String pointer to the data that needs to be validated
+   *         dosent need to contain any type of json
+   * After : checks if the key is points to a unvalidated user, if so it stores the user in neo4j and sets its status as validated  */
+  @RequestMapping(path = "/validation/{key}", method = RequestMethod.PUT, headers = "Accept=application/json")
+  public ResponseEntity<String> validateUser(@PathVariable String key){
+	HttpResponseBody clientResponse = new HttpResponseBody(); // create a instance of the response body
+    
+    // start by checking if the key exists if it dosent we responde with an not found error
+    if(!this.redisService.userNameExists(key)) {
+      clientResponse.addSingleError("error","User not found or validation period has expired please register again");
+      return new ResponseEntity<>(clientResponse.getErrorResponse(), HttpStatus.NOT_FOUND);
+    }
+    // now that we know the data exists we fetch it and move it into long term storage
+    JSONObject tempUrs = this.redisService.getAndDestroyData(key); // fetch the data and remove it from json
+    System.out.println(tempUrs.toString());
+    
+    // create a new User that will be instert into neo4j
+    User newuser = new User(tempUrs.getString("userName"),tempUrs.getString("password"),
+    		                tempUrs.getString("displayName"),tempUrs.getString("email"));
+    
+    // create the user in our neo4j databse
+    this.userService.createUser(newuser);
+    
+    // Return a happy message to the client telling him he can log in now
+    clientResponse.addSingleSucc("success","Validation succesful welcome to VeryWowChat go ahead login");
+    return new ResponseEntity<>(clientResponse.getSuccessResponse(), HttpStatus.CREATED);
+  }
+  
 }
