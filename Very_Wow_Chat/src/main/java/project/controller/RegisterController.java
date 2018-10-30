79d05c0969bdc5d99b5bc36d31c88975d137f99e
Roman <roman.rumba@hotmail.com>
2018-10-30 21:39:44 +0000

    finpussadi registerController

--- Very_Wow_Chat/src/main/java/project/controller/RegisterController.java
+++ Very_Wow_Chat/src/main/java/project/controller/RegisterController.java
@@ -4,6 +4,7 @@ import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,7 @@ public class RegisterController {
    *                                              "email":"shitufck@gmail.com"}
    * After : Validates the Client POST request and responds with an appropriate status code along with data */
   // value is endpoint , mothen is to what method this response to , headders denotes what are we returning
+  @CrossOrigin(origins = "http://localhost:3000")
   @RequestMapping(value="/register", method = RequestMethod.POST, headers = "Accept=application/json")
   public ResponseEntity<String> register(@RequestBody UserRegistrationFormReceiver payload) throws Exception {  
 	HttpResponseBody clientResponse = new HttpResponseBody(); // create a instance of the response body
@@ -56,18 +58,23 @@ public class RegisterController {
     /* before starting authenticating we check if the client can have this username
        i decided if the username is taken there is no reason to validate rest of the data */
     if(authenticator.userNameExists(payload.getUserName())) {
-      clientResponse.addToErrorArray("errors", "error", "Username already exists");
+      clientResponse.addErrorForForm("Username", "Username already exists");
       return new ResponseEntity<>(clientResponse.getErrorResponse(), HttpStatus.BAD_REQUEST);
     }
     
     // now that we know we have all the data we need and we know the username is legit we can start validating the data
     //We start calling out validator to validate the information and if there is a problem we take note of it
     if(!authenticator.passwordsMach(payload.getPassword(), payload.getPasswordReap())) {
-      clientResponse.addToErrorArray("errors", "error", "Both passwords must match");
+      clientResponse.addErrorForForm("Password", "Both passwords must match");
     }
     if(!authenticator.validatePass(payload.getPassword())) {
-      clientResponse.addToErrorArray("errors", "error", "Password must be  atleast 8 characters long"
-      		+ "have 1 digit,lower,uppercase,specialcase letter and cannot contain spaces/tabs");
+      clientResponse.addErrorForForm("Password", "Must be atleast 8 characters long");
+      clientResponse.addErrorForForm("Password", "Must contain a upper and a lowercase letter");
+      clientResponse.addErrorForForm("Password", "Must contain a specialcase letter");
+      clientResponse.addErrorForForm("Password", "Cannot contain spaces or tabs");
+    }
+    if(!authenticator.validEmail(payload.getEmail())) {
+      clientResponse.addErrorForForm("Email", "Email must be of valid form");
     }
     
     // if errors exists then we return the errors as the response
@@ -103,6 +110,7 @@ public class RegisterController {
    *         key is a String pointer to the data that needs to be validated
    *         dosent need to contain any type of json
    * After : checks if the key is points to a unvalidated user, if so it stores the user in neo4j and sets its status as validated  */
+  @CrossOrigin(origins = "http://localhost:3000")
   @RequestMapping(path = "/validation/{key}", method = RequestMethod.PUT, headers = "Accept=application/json")
   public ResponseEntity<String> validateUser(@PathVariable String key){
 	HttpResponseBody clientResponse = new HttpResponseBody(); // create a instance of the response body
@@ -122,8 +130,7 @@ public class RegisterController {
     
     // create the user in our neo4j databse
     this.userService.createUser(newuser);
-    
-    return new ResponseEntity<>(clientResponse.getSuccessResponse(), HttpStatus.NO_CONTENT);
+    return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
   }
   
 }
